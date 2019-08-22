c053886711f0b30b7dc1faeab0eba744db56d0df
Solovar <enigmadk.02@gmqail.com>
2019-08-22 23:21:07 +0200

    fixed some file ordering and naming conventions improved on the MakeVisual class, the build order is now partially compleat

--- classes/MakeVisual.py
+++ classes/MakeVisual.py
@@ -15,13 +15,21 @@ class MakeVisual:
         pass
 
     def _lunch_gui(self, title):
-        webview.create_window(title, self._blocks["compiled"])
-        webview.start()
+        pass
+        # webview.create_window(title, self._blocks["compiled"])
+        # webview.start()
 
     def build(self, title):
         build_dirs = ("../javaScript", "../html", "../css")
+        order = ('JS', 'body', 'head')
         # need to figur out build/constructing logic
-        self._lunch_gui(title)
+        for i, val in enumerate(build_dirs):
+            for item in listdir(val):
+                f = open(val + '/' + item, "r")
+                self._blocks[order[i]] = self._blocks[order[i]] + str(f.read())
+                f.close()
+
+        return self._lunch_gui(title)
 
     def get_block(self):
         return self._blocks
@@ -30,4 +38,6 @@ class MakeVisual:
         return self._data
 
 
-print(listdir(mypath))
+x = MakeVisual()
+print(x.get_block())
+x.build('title')
