c3bf4b5924e41eb8ac973768cdc8d3ae365f99c9
Solovar <enigmadk.02@gmail.com>
2019-08-29 14:29:58 +0200

    working the MakeVisual class, slowly getting to know pyWebView

--- classes/MakeVisual.py
+++ classes/MakeVisual.py
@@ -15,20 +15,33 @@ class MakeVisual:
         pass
 
     def _lunch_gui(self, title):
-        pass
-        # webview.create_window(title, self._blocks["compiled"])
-        # webview.start()
+
+        def evaluate_all(window):
+            window.load_css(self._blocks['head'])
+            result = window.evaluate_js(
+                str(self._blocks["JS"])
+            )
+
+        window = webview.create_window(title, html=str(self._blocks["compiled"]), min_size=(800, 500), confirm_close=True, background_color='#FFF', text_select=True)
+        webview.start(evaluate_all, window)
+
+    def _compile(self):
+        self._blocks['compiled'] += '<!DOCTYPE html><html lang="en">'
+        self._blocks['compiled'] += '<div class="container">'
+        self._blocks['compiled'] += '<body>' + self._blocks['body'] + '</body>'
+        self._blocks['compiled'] += '</div>'
+        self._blocks['compiled'] += '</html>'
 
     def build(self, title):
         build_dirs = ("../javaScript", "../html", "../css")
         order = ('JS', 'body', 'head')
-        # need to figur out build/constructing logic
         for i, val in enumerate(build_dirs):
             for item in listdir(val):
                 f = open(val + '/' + item, "r")
                 self._blocks[order[i]] = self._blocks[order[i]] + str(f.read())
                 f.close()
 
+        self._compile()
         return self._lunch_gui(title)
 
     def get_block(self):
@@ -39,5 +52,10 @@ class MakeVisual:
 
 
 x = MakeVisual()
-print(x.get_block())
+
 x.build('title')
+exit()
+for i in x.get_block():
+    print('<-------- ' + str(i) + " block")
+    print(x.get_block()[i])
+    print('<-------- end of ' + str(i) + " block")
