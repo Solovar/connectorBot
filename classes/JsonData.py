2460a8d2f339516db938f85b0e3de4636a06790c
Solovar <enigmadk.02@gmail.com>
2019-08-30 13:58:52 +0200

    started making logic for how json setting and information is handled

---
+++ classes/JsonData.py
@@ -0,0 +1,23 @@
+import json
+from os import listdir
+
+
+class JsonData:
+
+    def __init__(self):
+        self._error = False
+
+    def _pull(self):
+        pass
+
+    def _save(self):
+        pass
+
+    def get(self, get):
+        pass
+
+    def update(self, update, where):
+        pass
+
+    def error(self):
+        pass
