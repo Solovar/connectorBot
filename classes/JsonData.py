779862cdd547a37a8ebe286d3a818e2d5c78673e
Solovar <enigmadk.02@gmail.com>
2019-09-02 12:47:22 +0200

    started making logic for how json setting and information is handled

--- classes/JsonData.py
+++ classes/JsonData.py
@@ -3,21 +3,60 @@ from os import listdir
 
 
 class JsonData:
+    __instance = None
+    @staticmethod
+    def getinstance():
+        if JsonData.__instance is None:
+            JsonData()
+        return JsonData.__instance
 
     def __init__(self):
-        self._error = False
+        if JsonData.__instance is not None:
+            raise Exception("this class is a singleton")
+        else:
+            JsonData.__instance = self
 
-    def _pull(self):
+        self.__error = False
+        self.__dir = {}
+
+        for i in listdir('../Json'):
+            name = i[:-5]
+            with open('../Json/' + i) as json_data:
+                item = json.load(json_data)
+                self.__dir[str(name)] = item
+
+    def __pull(self, file):
         pass
 
-    def _save(self):
-        pass
+    def __save(self, path, data):
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
+
+    def get_all(self):
+        return self.__dir
 
     def get(self, get):
-        pass
+        try:
+            path = str(get).split('/')
+            storage = self.__dir
+            for i in path:
+                storage = storage[i]
 
-    def update(self, update, where):
+            return storage
+        except KeyError:
+            self.__error = "empty or worng path"
+
+    def update(self, data, path):
         pass
 
     def error(self):
-        pass
+        return self.__error
+
+
+s = JsonData.getinstance()
+x = s.get("twitter")
+
+if not s.error():
+    print(x)
+else:
+    print(s.error())
