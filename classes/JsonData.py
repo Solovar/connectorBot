38b5124cf03fee333d53e408734ff4a1684fe487
Solovar <enigmadk.02@gmail.com>
2019-09-02 14:33:39 +0200

    working more on the json handler

--- classes/JsonData.py
+++ classes/JsonData.py
@@ -18,7 +18,10 @@ class JsonData:
 
         self.__error = False
         self.__dir = {}
+        self.__result = None
+        self.__pull_all()
 
+    def __pull_all(self):
         for i in listdir('../Json'):
             name = i[:-5]
             with open('../Json/' + i) as json_data:
@@ -28,35 +31,40 @@ class JsonData:
     def __pull(self, file):
         pass
 
-    def __save(self, path, data):
-        with open(path, 'w') as outfile:
+    def __save(self, file_path, data):
+        with open(file_path, 'w') as outfile:
             json.dump(data, outfile)
 
     def get_all(self):
-        return self.__dir
+        self.__result = self.__dir
 
-    def get(self, get):
+    def get(self, path):
         try:
-            path = str(get).split('/')
+            item = str(path).split('/')
             storage = self.__dir
-            for i in path:
+            for i in item:
                 storage = storage[i]
 
-            return storage
+            self.__result = storage
         except KeyError:
             self.__error = "empty or worng path"
 
+    def result(self):
+        return self.__result
+
     def update(self, data, path):
-        pass
+        dir_look_up = str(path).split('/')
+        file = "../json/" + dir_look_up[0] + ".json"
+        self.__save(data, file)
+        # self.__pull()
 
     def error(self):
         return self.__error
 
 
-s = JsonData.getinstance()
-x = s.get("twitter")
+JsonData.getinstance().get('test')
 
-if not s.error():
-    print(x)
+if not JsonData.getinstance().error():
+    print(JsonData.getinstance().result())
 else:
-    print(s.error())
+    print(JsonData.getinstance().error())
