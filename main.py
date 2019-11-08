16a5a9093c289e6669e3abbe6a2466d7904e85b5
Solovar <enigmadk.02@gmail.com>
2019-11-08 13:52:23 +0100

    updated the visuals to use JinJa2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import Flask, render_template
 from flaskwebgui import FlaskUI  # get the FlaskUI class
 from classes.MakeVisual import MakeVisual
 from classes.JsonData import JsonData
@@ -16,60 +17,43 @@ json = JsonData.getinstance('static/Json')
 
 @app.route("/")
 def index():
-    x = MakeVisual()
+    # route = request.url_rule
+    # print(route)
     """data = {"persons": {'anna': {"age": 20}}}
     json.update(data, "test")"""
-    x.add_html_from_file('console')
-    return x.printing()
+    return render_template('console.html')
 
 
 @app.route("/Twitter")
 def twitter():
-    x = MakeVisual()
+    route = str(request.url_rule)
     """data = {"persons": {'anna': {"age": 100}}}
     json.update(data, "test")"""
-    x.add_html_from_file('twitter')
-    return x.printing()
-
-
-@app.route("/Twitter", methods=['POST'])
-def twitter_post():
-    x = MakeVisual()
-    x.add_html_from_file('twitter')
-    return x.printing()
+    return render_template('twitter.html', route=route)
 
 
 @app.route("/Twitter/Auto")
 def tweet_auto():
-    x = MakeVisual()
-    x.add_html_from_file('twitter-auto')
-    return x.printing()
+    route = str(request.url_rule)
+    return render_template('twitter-auto.html', route=route)
 
 
 @app.route("/Twitter/New")
 def tweet_new():
-    x = MakeVisual()
-    x.add_html_from_file('twitter-new')
-    return x.printing()
+    route = str(request.url_rule)
+    return render_template('twitter-new.html', route=route)
 
 
 @app.route("/Settings")
 def settings():
-    x = MakeVisual()
-    x.add_script('settings.js')
-    x.add_html_from_file('settings')
-    return x.printing()
+    return render_template('settings.html')
 
 
 @app.route("/AJAX/Settings")
 def ajaxed_settings():
-    x = MakeVisual()
     json.get("test/persons")
-    return_data = ''
-    for key, val in json.result().items():
-        return_data += "<li>" + key + " is: " + str(val['age']) + " years old</li>"
-    x.add_plain_html(return_data)
-    return x.ajax_priting()
+    json_data = json.result().items()
+    return render_template('ajax_settings.html', data=json_data)
 
 
 @app.route("/AJAX/show_tweets")
