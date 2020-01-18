ea24069a33a16b7d3bd688522d1bf29b2eb62d66
Solovar <enigmadk.02@gmqail.com>
2020-01-18 02:56:03 +0100

    working on the music player

--- musicPlayer.py
+++ musicPlayer.py
@@ -1,16 +1,12 @@
 import pafy
-import vlc
-
-#https://stackoverflow.com/questions/49354232/stream-youtube-audio-from-python-using-url-without-downloading-python
-
-url = "https://www.youtube.com/watch?v=bMt47wvK6u0"
-video = pafy.new(url)
-best = video.getbest()
-playurl = best.url
-
-Instance = vlc.Instance()
-player = Instance.media_player_new()
-Media = Instance.media_new(playurl)
-Media.get_mrl()
-player.set_media(Media)
-player.play()
+import tkinter
+import threading
+import time
+import codecs
+import sys
+from random import shuffle
+from os import chdir, getcwd
+from vlc import Instance, State
+from tkinter import messagebox
+from requests import get
+from bs4 import BeautifulSoup
